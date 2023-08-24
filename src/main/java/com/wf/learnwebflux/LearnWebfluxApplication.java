67b6247d22ac0a60f17319dfaa02a44f5c028864
sypatrick <booth20037@gmail.com>
2023-08-24 13:59:59 +0900

    first-commit

---
+++ src/main/java/com/wf/learnwebflux/LearnWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.wf.learnwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnWebfluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wf/learnwebflux/LearnWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wf.learnwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
